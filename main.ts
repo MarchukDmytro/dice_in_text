2993f4398ec4961c5ce7272e667832595d7e2aa2
dima marchuk <dimanoidwork@gmail.com>
2025-08-21 01:28:02 +0200

    plugin finished

--- main.ts
+++ main.ts
@@ -1,134 +1,116 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, Notice } from "obsidian";
+import { Decoration, EditorView, ViewPlugin, ViewUpdate } from "@codemirror/view";
+import { RangeSetBuilder } from "@codemirror/state";
 
-// Remember to rename these classes and interfaces!
+export default class DicePlugin extends Plugin {
+	onload() {
+		console.log("Loading Dice Plugin");
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		// Reading view (preview)
+		this.registerMarkdownPostProcessor((el) => {
+			const diceRegex = /(\d+[dк]\d+([\+\-]\d+)?)/ig;
+			el.querySelectorAll("p, li").forEach((block) => {
+				block.innerHTML = block.innerHTML.replace(diceRegex, (match) => {
+					return `<span class="clickable-dice" data-dice-formula="${match}">${match}</span>`;
+				});
+			});
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// Live Preview (editor)
+		this.registerEditorExtension(this.createLivePreviewPlugin());
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+		// Handle clicks globally
+		document.addEventListener("click", (evt) => {
+			const target = evt.target as HTMLElement;
+			if (target.classList.contains("clickable-dice")) {
+				const formula = target.getAttribute("data-dice-formula");
+				if (formula) {
+					const rollResult = this.rollDice(formula);
+
+					// Create styled notice
+					const notice = new Notice(rollResult.text, 8000);
+					// Add CSS class for criticals
+					if (rollResult.cssClass) {
+						notice.noticeEl.classList.add(rollResult.cssClass);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
 			}
 		});
+	}
+		 
+	rollDice(formula: string): string {
+		const match = formula.match(/(\d+)[dк](\d+)([+-]\d+)?/i);
+		if (!match) return "Invalid dice formula";
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		const num = parseInt(match[1], 10);
+		const sides = parseInt(match[2], 10);
+		const modifier = match[3] ? parseInt(match[3], 10) : 0;
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		let rolls: number[] = [];
+		let baseSum = 0;
+		for (let i = 0; i < num; i++) {
+			const roll = Math.floor(Math.random() * sides) + 1;
+			rolls.push(roll);
+			baseSum += roll;
+		}
+		const finalSum = baseSum + modifier;
+
+		// Pretty breakdown
+		let noticeText = `🎲 ${formula}\n`;
+		noticeText += `Rolls: [${rolls.join("] [")}]\n`;
+		noticeText += `Sum: ${baseSum}`;
+		if (modifier !== 0) {
+			noticeText += ` ${modifier > 0 ? "+" : "-"} ${Math.abs(modifier)}`;
+		}
+		noticeText += ` = ${finalSum}`;
+
+				// Detect critical success/failure
+		let cssClass: string | undefined;
+		if (formula.toLowerCase() === "1d20") {
+			if (rolls[0] === 20) cssClass = "dice-critical-success";
+			if (rolls[0] === 1) cssClass = "dice-critical-fail";
+		}
+
+		return { text: noticeText, cssClass };
+	}
+
+	createLivePreviewPlugin() {
+		const diceRegex = /(\d+[dк]\d+([\+\-]\d+)?)/ig;
+
+		return ViewPlugin.fromClass(class {
+			decorations;
+
+			constructor(view: EditorView) {
+				this.decorations = this.buildDecorations(view);
+			}
+
+			update(update: ViewUpdate) {
+				if (update.docChanged || update.viewportChanged) {
+					this.decorations = this.buildDecorations(update.view);
+				}
+			}
+			buildDecorations(view: EditorView) {
+				const builder = new RangeSetBuilder<Decoration>();
+				for (let { from, to } of view.visibleRanges) {
+					const text = view.state.doc.sliceString(from, to);
+					let match;
+					while ((match = diceRegex.exec(text)) !== null) {
+						const start = from + match.index;
+						const end = start + match[0].length;
+						builder.add(
+							start,
+							end,
+							Decoration.mark({
+								class: "clickable-dice",
+								attributes: { "data-dice-formula": match[0] }
+							})
+						);
+					}
+				}
+				return builder.finish();
+			}
+		}, {
+			decorations: v => v.decorations
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
